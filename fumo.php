3cf37f6211991a6b87684b80f893c5711b8ab54c
sudonona <80674485+sudonona@users.noreply.github.com>
2021-07-06 13:13:47 +0200

    Add files via upload

---
+++ fumo.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang = "it">
+  <head>
+    <title>Dipendenza dal fumo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href = "css/fumo.css">
+
+    <style>
+        h1 {color:brown;}
+        h2 {color:brown;}
+</style>
+  </head>
+  <body>
+  <div class="header">
+    <h1> Dipendenza Dal Fumo</h1>
+  </div>
+
+  <?php include 'navbar.php';?>
+
+  <div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+    <blockquote>
+      <h1><b>Danni Causati dal Fumo :</b></h1>
+
+      <p>Malattie respiratorie, infarti e cardiopatie gravi. Questi e molti altri sono i danni del fumo. Smettere è l’unica soluzione per restare in salute.</p><br>
+      
+      <h2>Quali danni provoca il fumo ?</h2>
+
+      <p> Il vizio del fumo è uno dei principali fattori che provocano problemi di salute con conseguenze dannose, e nello specifico le patologie correlate riguardano:</p>
+      <p><b><i>Malattie cardio-circolatorie :</b></i> il fumo riduce il trasporto di ossigeno ai tessuti e fa aumentare la pressione arteriosa e la frequenza cardiaca. Questo danneggia cuore e arterie, favorendo lo sviluppo di malattie cardiovascolari come aritmie, infarto al miocardio, cardiopatia ischemica e ictus;</p>
+      <p><b><i>Patologie respiratorie e infezioni broncopolmonari :</b></i> le sostanze tossiche del fumo una volta assorbite dai polmoni possono favorire l’insorgenza di asma, BPCO, enfisema, apnee ostruttive, insufficienza respiratoria, fibrosi polmonare e tumore al polmone.</p>
+      <p>Ma non solo. Gli effetti nocivi del fumo tendono ad accumularsi nel tempo e provocare altresì:<br>
+      <p><b><i>Invecchiamento della pelle :</b></i> fumare causa il rilascio nell’organismo di radicali liberi che ostacolano la produzione di collagene e comportano perdita di elasticità, comparsa di macchie cutanee e la formazione di rughe soprattutto nella regione intorno alle labbra;</p>
+      <p><b><i>Effetti sul cavo orale :</b></i> diminuisce le difese immunitarie nei confronti della placca batterica favorendo l’ingiallimento della dentina, gengiviti, alitosi e l'insorgenza del cancro alla bocca;</p>
+      <p><b><i>Danni sulla sessualità maschile :</b></i> la sigaretta avendo un effetto dannoso a livello vascolare, è un fattore di rischio importantissimo nello sviluppo della disfunzione erettile;</p>
+      <p><b><i>Problemi in gravidanza :</b></i> il fumo influisce negativamente sull'apparato riproduttivo femminile, aumenta il rischio di aborti e provoca menopause più precoci.</p>
+      Le sostanze nocive presenti nella sigaretta (nicotina, catrame, monossido di carbonio, ammoniaca, ecc.) danneggiano l'organismo con diverse azioni:<br>
+      <p><b><i>Cancerogena</b></i> (sviluppo e accrescimento del cancro del polmone, del cavo orale, della laringe, dell'esofago, del pancreas, della vescica e del rene);</p>
+      <p><b><i>Irritante </b></i>(tosse, bronchite cronica, enfisema);</p>
+      <p><b><i>Ossidante</b></i>(infarto cardiaco, ictus cerebrale, emorragia cerebrale, trombosi, invecchiamento precoce). </p>
+      <p>Non esiste quindi una soglia di sicurezza sotto la quale il fumo non produce danni..</p><br>
+      
+      <h2><b>I danni del fumo passivo</b></h2>
+
+      <p>Attenzione perché anche il fumo passivo è considerato irritante e cancerogeno per l'uomo. Infatti, è ben nota la correlazione tra il fumo passivo e l'insorgenza di malattie cardiovascolari e di cancro nei non fumatori.</p>
+      <p>L’esposizione passiva al tabacco fumato da altri e, in generale, frequentare ambienti inquinati, è responsabile di 1/4 delle malattie respiratorie e particolarmente dannoso soprattutto per neonati e bambini.</p><br>
+     
+      <h2><b>I vantaggi dello smettere di fumare</b></h2>
+
+        <p>Smettere di fumare è certamente uno dei passi più importanti che si possono fare per ridurre l'insorgenza di malattie e allungare la speranza di vita. Infatti:</p>
+        <p>La pressione del sangue e la frequenza del battito cardiaco tornano normali e il rischio di avere un infarto cardiaco si riduce del 50% dopo 1 anno, mentre il rischio di ictus cerebrale regredisce in 5-10 anni dall’astensione;</p>
+        <p>Il rischio di cancro del polmone è paragonabile a quello di chi non ha mai fumato dopo 10 anni dalla sospensione.
+        Inoltre, smettere di fumare migliora la qualità della vita:</p>
+        <p> Olfatto e gusto migliorano già dopo alcuni giorni;</p>
+        <p> La pelle ritorna più luminosa dopo alcune settimane;</p>
+        <p> I denti diventano più bianchi;</p>
+        <p> L'alito diventa più gradevole;</p>
+        <p> Gli abiti e i capelli non hanno più l'odore di fumo;</p>
+        <p> Il respiro migliora e scompare la tosse da fumo;</p>
+        <p> Si salgono più facilmente le scale;</p>
+        <p> Ci si sente meglio risparmiando anche soldi.</p>
+        </blockquote>
+    </div>
+    <div class="card">
+    <blockquote>
+      <h1><b>Come smettere di fumare: metodi e tecniche</b></h1>
+
+      <p>Il primo strumento per smettere di fumare? La forza di volontà ma, se non dovesse essere sufficiente, ecco una serie di consigli per dire no al fumo.</p>
+
+      <p>La vita è una resistenza continua all’inerzia che tenta di sabotare il nostro volere più profondo. Chi si stanca di volere, vuole il nulla”, scriveva il filosofo tedesco Friedrich Nietzsche. Una frase che calza a pennello a chi, con grande forza di volontà, sta cercando di <b>smettere di fumare</b>, trovandosi - ed è inevitabile - ad affrontare un vero e proprio momento di cambiamento.</p>
+      <p><b>Un passaggio contraddistinto da due fasi: una di preparazione, una di azione.</b> Volere è potere, recita il detto. Ma non è così semplice, perché quando si è abituati a fumare si tende a perdere di vista i vantaggi della vita senza sigarette e diventa difficile tornare indietro. A lungo andare, il rischio è di perdere la motivazione per liberarsi dal tabagismo, dimenticando che <b>in realtà non esiste alcuna buona ragione per fumare.</b></p>
+      </blockquote>
+
+      <br><blockquote>
+      <h2><b>Smettere di fumare si può (e si deve)</b></h2>
+      <div class="gallery" >
+          <a target ="blank" href = "css/immagini/smetto.jpg">
+            <img src ="css/immagini/smetto.jpg" alt="Ottica" width="600" height="400">
+          </a>
+      </div>
+      <p><b>Sono centinaia, infatti, le sostanze tossiche rilasciate quando si accende una sigaretta.</b> Fumare rappresenta una delle prime cause evitabili di morte prematura: questo è un dato di fatto, così come gli effetti negativi del fumo sulla salute. Secondo i dati pubblicati dall’Organizzazione Mondiale della Sanità, nel mondo il tabacco è responsabile di un numero maggiore di vittime rispetto a quelle provocate da alcol, droghe, AIDS, incidenti stradali, omicidi e suicidi messi insieme.</p>
+      <p>Si calcola che ogni anno, a livello mondiale, <b>si potrebbero evitare circa 6 milioni di morti soltanto abolendo l’uso del tabacco</b>; in Italia si stima che le morti ascrivibili al fumo vadano da 70.000 a 83.000 ogni anno. Va ricordato, inoltre, che oltre un quarto delle vittime del fumo ha un’età compresa tra i 35 e i 65 anni.</p>
+
+      <br><h2><b>I consigli per smettere di fumare</b></h2>
+
+      <p>Ad ogni modo, come si è deciso di fumare si può decidere di smettere. Le statistiche riportano che abbandonare le sigarette non è un obiettivo facile da raggiungere, ma a disposizione di chi fa questa scelta - attualmente - ci sono una serie di strumenti sui quali si può fare affidamento. Di seguito una serie di consigli per smettere di fumare:</p>
+      <p><b><i>Counselling:</i></b> numerosi studi dimostrano che il solo consiglio di smettere di fumare, da parte del proprio medico, fa in modo che un determinato numero di persone lo segua. Talvolta i fumatori possono partecipare a gruppi di auto-mutuo-aiuto per smettere di fumare.</p>
+      <p><b><i>Centri antifumo:</i></b> in Italia sono presenti centinaia di centri antifumo (tutte le informazioni sono sul sito dell’Istituto Superiore di Sanità) che, per aiutare a smettere di fumare, ricorrono a vari strumenti: counselling individuale e trattamento di gruppo per smettere di fumare, ma anche prescrizione di prodotti sostitutivi della nicotina oppure di farmaci per la disassuefazione.</p>
+      <p><b><i>Farmaci:</i></b> in alcuni casi il medico, accanto al supporto psicologico e all’aiuto comportamentale, può prescrivere antidepressivi oppure altri farmaci in grado di aiutare il paziente a superare il momento più complicato, quello del distacco dalla sigaretta.</p>
+      <p><b><i>Trattamenti sostitutivi:</i></b>cerotti, chewing gum e altri prodotti a base di nicotina. Cercare di smettere di fumare equivale a confrontarsi con la dipendenza psicologica da tabacco che si esprime (anche) nel piacere e nella gestualità del fumare, in particolar modo attraverso disturbi causati dall’astinenza da nicotina, una sostanza che il corpo è avvezzo ad assumere.</p>
+      <p><b><i>Sigarette elettroniche:</i></b> in merito ai rischi e benefici delle sigarette elettroniche (alcuni anni fa si è registrato un vero e proprio boom di vendite di sigarette elettroniche), si è discusso molto. Gli studi condotti fino a questo momento non consentono di escludere possibili danni legati a questo nuovo modo di fumare.</p>
+      <p><b><i>Libri per smettere di fumare:</i></b> gli scaffali delle librerie sono pieni di manuali per smettere di fumare. In alcuni casi la lettura di questi volumi può aiutare concretamente, o quantomeno stimolare il desiderio di smettere di fumare in chi non ci pensava proprio.</p>
+      <p>Va da sé che ancora oggi la forza di volontà rimane il primo, indispensabile “strumento” per dire addio al fumo e alle sigarette. Certo, la nicotina produce dipendenza, ed è normale ricadere nel vizio del fumo dopo qualche giorno (oppure settimana) di astinenza dal fumo. In questi casi ciò che conta è non scoraggiarsi, ritentare finché non si riesce a smettere, magari facendosi aiutare a prendere questa difficile decisione una volta per tutte e a realizzarla in concreto.</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>About Us</h2>
+      <p>Siamo una StartUp composta da 03 giovani studenti dell'università di GENOVA che crede ancora che l'unione e la solidarietà sono le chiave per vincere qualsiasi difficoltà sociale, academico ma soppratutto SANITARIA...<a href = "aboutUs.php">Leggi di più</a></p>
+    </div>
+    <div class="card">
+      <h2>Gruppo di sostegno</h2>
+      <div class="gallery" >
+          <a target ="blank" href = "css/immagini/persone.jpeg">
+            <img src ="css/immagini/persone.jpeg" alt="Ottica" width="100%" height="200px">
+          </a>
+          <p>I gruppi di sostegno al fumo sono percorsi per smettere di fumare che hanno come finalità quella di accompagnare i fumatori ad abbandonare l’abitudine al fumo ma anche, più in generale, nel prendersi cura della propria salute adottando stili di vita più sani.</p>
+          <p>Gli incontri di gruppo sono strutturati come un vero e proprio percorso in cui il confronto e la condivisione con i diversi partecipanti sostiene notevolmente la motivazione allo smettere di fumare. La metodologia utilizzata dal nostro team è di tipo comunicazionale e non prevede l’indicazione di utilizzo di sussidi farmacologici.</p>
+          <p>Il percorso viene attivato senza condizione di partecipanti minimi ossia non ci deve essere un minimo di persone e di solito si svolge in presenza ma da dicembre 2020 sono state studiate nuove modalità di svolgimento rispondenti all’emergenza coronavirus.</p>
+      </div>
+      <div class="gallery" >
+      <h2> Volontariato </h2>
+          <a target ="blank" href = "css/immagini/volontario.jpg">
+            <img src ="css/immagini/volontario.jpg" alt="volontariato" width="100%" height="200px">
+          </a>
+          <p><i>Perchè “… quella singola offerta ti verrà restituita in molti giorni, ti verrà rimborsata incalcolabilmente di più. Ecco che questi versi raccontano della economia sovversiva del dono, del gratis, dello spariglio che riceve in cambio una restituzione gigantesca. Questa è l’economia del dono che butta gambe all’aria i pareggi di bilancio, le partite doppie dare e avere, grazie al gratis, si tratta del dono da vita a vita.” (Erri De Luca, Il turno di notte lo fanno le stelle Feltrinelli 2012)</i><p>
+          <p>Fare bene agli altri fa bene a te stesso. Donaci un pò del tuo tempo come volontario.</p>
+          <p>Tutte le attività da noi offerti sono rese possibili dalla preziosa e costante attività dei volontari, i quali mettono il loro tempo al servizio della nostra azione di solidarietà e dei pazienti. È soprattutto grazie all’impegno dei volontari che possiamo offrire un ventaglio di servizi costanti nel tempo e che riusciamo ad essere presenti in modo continuo. Il gruppo dei volontari si suddivide in sotto-gruppi con esperienza appositamente per i diversi ambiti di attività.</p>
+        
+        </div>
+      <div class="gallery" >
+      <h2> Testimonianza  </h2>
+          <a target ="blank" href = "css/immagini/stop.jpg">
+          <img src ="css/immagini/stop.jpg" alt="smetto" width="100%" height="200px">
+          </a>
+          <p><i>"Ho iniziato a fumare regolarmente da quando avevo 15 anni per sentirmi 'grande', ed è stato attraverso uno screening che ho scoperto di avere un nodulo al polmone. Ma, in realtà, non avevo alcun sintomo che lo facesse presagire".</i></p>
+          <p><i>Grazie al gruppo di sostegno disponibile 24ore su 24 ogni giorno, con un sistema di comunicazione senza giudizi e con pazienta ora sono 2anni con non tacco un sigaretta di qualsiasi tipo.</i></p>
+      </div>
+    </div>
+    <div class="card">
+      <h2><bold>Contattaci</bold></h2>
+      <p>Voui maggiore informazioni ?
+        <a class="btn btn-primary" href="indexMail.php" type="submit"> Invia una mail</a></p></p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <h3>copyright&copy 2021-All Right Reserved</h3>
+</div>
+
+</body>
+</html>
+
+
+
+
